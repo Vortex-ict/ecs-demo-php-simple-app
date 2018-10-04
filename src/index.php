04757143540f53e98b41d98afc2f6213c9f4ba80
Rene Edward Vrijenhoek <renev@MacBook-Pro-2.local>
2018-10-04 11:10:01 +0200

    bugfix05, in index.php

--- src/index.php
+++ src/index.php
@@ -19,6 +19,7 @@
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS of RV.</p>
                 <p>Your PHP application is now running on a VOPAK ECS environment. It WORKS!!!.</p>
+                <p>Morgenochtend staat er wat anders op deze website :-).</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
